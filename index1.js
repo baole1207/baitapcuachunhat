7ccd2981f01bc6da56336b587291c1d4733bf3df
baole1207 <edmmix135@gmail.com>
2022-11-08 00:49:46 +0700

    first commit

---
+++ index1.js
@@ -0,0 +1,62 @@
+// bt1
+// let a;
+// a = prompt("Nhập vào số a");
+// a = parseInt(a);
+// if (a % 2 == 0){
+//     document.write("a là số chẵn");
+// }
+// else{
+//     document.write("a là số lẽ");
+// }
+
+// bt2
+// let b;
+// let c;
+// a =prompt("nhap a") 
+// b =prompt("nhap b") 
+// c =prompt("nhap c") 
+// if (a>b) {
+//     if(a>c){
+// alert("a max")
+//     } else { alert ("c max")}
+// }
+//  else if(b>c){
+//     alert("b max")}
+//     else { alert ("c max")}
+
+// // bt3
+// let a ;
+//  a = prompt ("nhap vao mot so")
+//  var b = a;
+ 
+//  while( a >= 1){
+//     a = a - 1;
+//     b = b*a;
+//  }
+//  document.write(b);
+
+// // bt4
+// let a ;
+//  a = prompt ("nhap vao mot so")
+//  const list =[]
+//  while( a >= 1){  
+//     if( a % 2 == 0){
+//         list.push(a)
+//     }
+//     a = a-1
+//  }
+//  document.write(list);
+
+ 
+
+// bt5
+// em chịu ko bt cộng số dương 
+// var array_sum = function(my_array) {
+//     if (my_array.length === 1) {
+//       return my_array[0];
+//     }
+//     else {
+//       return my_array.pop() + array_sum(my_array);
+//     }
+//   };
+//   document.write(array_sum([1,2,3,4,5,6,-1]));
